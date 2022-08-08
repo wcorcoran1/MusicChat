73e1c07cb895c41006627a15bd34a186552eb5c3
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-08-08 12:01:17 -0500

    update and delete post api paths made

--- api/posts.js
+++ api/posts.js
@@ -21,36 +21,56 @@ postsRouter.get("/", loginAuth, async (req, res, next) => {
   }
 });
 
-postsRouter.get("/:postsId", loginAuth, async(req,res, next)=>{
-    try{
-        const { postsId } = req.params;
+postsRouter.get("/:postsId", loginAuth, async (req, res, next) => {
+  try {
+    const { postsId } = req.params;
     const posts = await getPostsById(postsId);
-    res.send(posts)
-    }catch(error){
-        throw error
-    }
-})
+    res.send(posts);
+  } catch (error) {
+    throw error;
+  }
+});
 
-postsRouter.get("/:genreId", loginAuth, async(req, res, next)=>{
-    try{
-const { genreId } = req.params
-const posts = await getPostsByGenre(genreId)
-res.send(posts)
-    }catch(error){
-        throw error
-    }
-})
+postsRouter.get("/:genreId", loginAuth, async (req, res, next) => {
+  try {
+    const { genreId } = req.params;
+    const posts = await getPostsByGenre(genreId);
+    res.send(posts);
+  } catch (error) {
+    throw error;
+  }
+});
 
-postsRouter.post("/", loginAuth, async(req, res, next)=>{
+postsRouter.post("/", loginAuth, async (req, res, next) => {
+  try {
+    const { content } = req.body;
+    const { authorId } = req.user;
+    const { genreId } = req.genre;
+    const post = await createPosts({ authorId, genreId, content });
+    res.send(post);
+  } catch (error) {
+    throw error;
+  }
+});
+
+postsRouter.patch("/:postsId", loginAuth, async (req, res, next) => {
+  const id = req.params.postsId;
+  const { content } = req.body;
+  try {
+    const updatedPosts = await updatePosts({ id, content });
+    res.send(updatedPosts);
+  } catch (error) {
+    throw error;
+  }
+});
+
+routineRouter.delete("/:postsId",loginAuth, async (req, res, next)=>{
+    const id = req.params
     try{
-        const { content } = req.body
-        const { authorId } = req.user
-        const { genreId } = req.genre
-        const post = await createPosts({ authorId, genreId, content })
-        res.send(post)
-    }catch(error){
-        throw error
-    }
-})
+        const deletePost = await deletePosts(id)
+        res.send(deletePost);
+    } catch(error){
+        throw(error)
+    }})
 
 module.exports = postsRouter;
